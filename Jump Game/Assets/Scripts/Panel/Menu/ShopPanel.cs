2fe5ecba28d9dfdf14fd4b09154866daef3a363f
Cin <ldcin2409@gmail.com>
2025-09-02 22:09:42 +0700

    feat: add Animation

--- Jump Game/Assets/Scripts/Panel/Menu/ShopPanel.cs
+++ Jump Game/Assets/Scripts/Panel/Menu/ShopPanel.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopPanel : Panel
 {
+    [SerializeField] private GameObject _characterCustom;
+    [SerializeField] private GameObject _backgroundCustom;
+    [SerializeField] private Button _characterTab;
+    [SerializeField] private Button _backgroundTab;
+    private Color activeRed = new Color(0.9f, 0.2f, 0.2f, 1f);
+    private Color inactiveRed = new Color(0.5f, 0.1f, 0.1f, 1f);
+
+    public void Start()
+    {
+        ShowCharacterPanel();
+    }
+    public void ShowCharacterPanel()
+    {
+        SoundManager.Instance.PlayClickSound();
+        _characterCustom.SetActive(true);
+        _backgroundCustom.SetActive(false);
+        SetTabColors(_characterTab, _backgroundTab);
+    }
+    public void ShowBackgroundPanel()
+    {
+        SoundManager.Instance.PlayClickSound();
+        _characterCustom.SetActive(false);
+        _backgroundCustom.SetActive(true);
+        SetTabColors(_backgroundTab, _characterTab);
+    }
+    private void SetTabColors(Button active, Button inactive)
+    {
+        active.image.color = activeRed;
+        inactive.image.color = inactiveRed;
+    }
     public void BackToMenu()
     {
         SoundManager.Instance.PlayClickSound();
